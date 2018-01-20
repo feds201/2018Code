77e2ea1938bbc038979a306dd1ebf8ba98144f75
feds201 <feds201@10.82.50.17>
2018-01-20 12:12:28 -0500

    Started Writing Auton.cpp finished Auton.h

--- 2018Code/src/Auton.cpp
+++ 2018Code/src/Auton.cpp
@@ -4,6 +4,16 @@
  *  Created on: Jan 16, 2018
  *      Author: feds201
  */
+#include"Auton.h"
+
+Auton::Auton(Drivetrain * drive){
+
+	list = new struct AutonList;
+
+	list->drive = drive;
+
+
+}
 
 
 
--- 2018Code/src/Auton.h
+++ 2018Code/src/Auton.h
@@ -8,7 +8,55 @@
 #ifndef SRC_AUTON_H_
 #define SRC_AUTON_H_
 
+#include"WPILib.h"
+#include"ctre/Phoenix.h"
+#include"Drivetrain.h"
+#include"Timer.h"
 
+
+class Auton{
+
+public:
+
+Auton(Drivetrain* drive);
+void Drive(double speed, double dist);
+void Rotate(double angle);
+void Arc(double speed, double dist, double radius);
+
+
+
+
+private:
+
+	struct AutonList{
+
+		Drivetrain* drive;
+
+		Timer timer;
+
+		double counterPerIn = 1950;
+		double Ptheta = 0;
+		double Pv = 0;
+
+		double Wc = 0;
+		double Vc = 0;
+		double Rc = 0;
+		double thetac = 0;
+		double thetaErr = 0;
+		double Vi = 0;
+		double Vo = 0;
+		double Si = 0;
+		double So = 0;
+		double Ra = 0;
+		double Va = 0;
+		double VErr = 0;
+
+
+	};
+
+	struct AutonList *list;
+
+};
 
 
 
